9477c475dc12cb6601e96de6db320bf1b109d37b
voxquoocshuyy <voquochuy1502@gmail.com>
2024-01-13 17:50:42 +0700

    feat: add base repository

--- src/BuildingBlocks/Contracts/obj/Debug/net6.0/Contracts.AssemblyInfo.cs
+++ src/BuildingBlocks/Contracts/obj/Debug/net6.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91774a3761870bf052c90a1a22f5035bc95a6eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc322028d3e6c97fb521969d3538ba171c5ddea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Services/Product.API/Extensions/ServiceExtensions.cs
+++ src/Services/Product.API/Extensions/ServiceExtensions.cs
@@ -1,7 +1,11 @@
+using Contracts.Common.Interfaces;
+using Infrastructure.Common;
 using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Product.API.Persistence;
+using Product.API.Repositories;
+using Product.API.Repositories.Interfaces;
 
 namespace Product.API.Extensions;
 
@@ -17,6 +21,7 @@ public static class ServiceExtensions
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
         services.ConfigureProductDbContext(configuration);
+        services.AddInfrastructureServices();
 
         return services;
     }
@@ -32,6 +37,14 @@ public static class ServiceExtensions
             e.MigrationsAssembly("Product.API");
             e.SchemaBehavior(MySqlSchemaBehavior.Ignore);
         }));
+        return services;
+    }
+    private static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+            .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+            .AddScoped<IProductRepository, ProductRepository>()
+            ;
         return services;
     }
 }
